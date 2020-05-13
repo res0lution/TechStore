709510b1956057d0ea7cf090454cf357ea7a9e85
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-13 07:56:30 +0300

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "./App.css";
+import { HomePage } from "./pages/HomePage"
+import { AboutPage } from "./pages/AboutPage";
+import { ContactPage } from "./pages/ContactPage";
+import { Products } from "./pages/Products";
+import { SingleProductPage } from "./pages/SingleProductPage";
+import { CartPage } from "./pages/CartPage";
+import { Default } from "./pages/Default";
+
 function App() {
-  return <div className="App">Tezt </div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id" component={SingleProductPage} />
+        <Route path="/cart" component={CartPage} />
+        <Route path="*" component={Default} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
