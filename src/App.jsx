ba97d2d2f97ffd37ededa2df43e35a1eb951916f
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-13 07:37:23 +0300

    Created Folder and Page structures

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Tezt</div>;
+  return <div className="App">Tezt </div>;
 }
 
 export default App;
---
+++ src/pages/SingleProductPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SingleProductPage = () => {
+    return (
+        <div>
+          SingleProductPage
+        </div>
+    )
+}
