9dcf46f54338672e5bbe09f7db0da0d3cc3a2c49
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-14 08:53:43 +0300

    Created Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+import { ProductConsumer } from "../context/context";
 
 export const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <ProductConsumer>
+      {(value) => {
+        return (
+          <FooterWrapper>
+            <div className="container py-3">
+              <div className="row">
+                <div className="col-md-6">
+                  <p className="text-capitalize">
+                    Copyright &copy; Tech store {new Date().getFullYear()}. All
+                    rights reserved
+                  </p>
+                </div>
+
+                <div className="col-md-6 d-flex justify-content-around">
+                  {value.socialIcons.map((item) => (
+                    <a key={item.id} href={item.url}>
+                      {item.icon}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </FooterWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
+};
+
+const FooterWrapper = styled.footer`
+  background: var(--darkGrey);
+  color: var(--mainWhite);
+
+  .icon {
+    color: var(--mainWhite);
+    font-size: 1.5rem;
+    transition: var(--mainTransition);
+  }
+
+  .icon:hover {
+    color: var(--primaryColor);
+  }
+`;
--- src/components/SideCart.jsx
+++ src/components/SideCart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 
 import { ProductConsumer } from "../context/context";
-import styled from "styled-components";
 
 export const SideCart = () => {
   return (
