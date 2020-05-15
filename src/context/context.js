0ba39ebddd707bd8b51b86eef1055206c2c6e6e4
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-15 10:01:24 +0300

    Created addToCart method

--- src/context/context.js
+++ src/context/context.js
@@ -37,7 +37,9 @@ class ProductProvider extends Component {
       return product;
     });
 
-    let featuredProducts = storeProducts.filter((item) => item.featured === true);
+    let featuredProducts = storeProducts.filter(
+      (item) => item.featured === true
+    );
 
     this.setState({
       storeProducts,
@@ -62,7 +64,33 @@ class ProductProvider extends Component {
 
   syncStorage = () => {};
 
-  addToCart = (id) => {};
+  addToCart = (id) => {
+    let tempCart = [...this.state.cart];
+    let tempProducts = [...this.state.storeProducts];
+    let tempItem = tempCart.find((item) => item.id === id);
+
+    if (!tempItem) {
+      tempProducts.find((item) => item.id === id);
+      let total = tempItem.price;
+      let cartItem = { ...tempItem, count: 1, total };
+      tempCart = [...tempCart, cartItem];
+    } else {
+      tempItem.count++;
+      tempItem.total = tempItem.price * tempItem.count;
+      tempItem.total = parseFloat(tempItem.total.toFixed(2));
+    }
+
+    this.setState(
+      () => ({
+        cart: tempCart,
+      }),
+      () => {
+        this.addTotals();
+        this.syncStorage();
+        this.openCart();
+      }
+    );
+  };
 
   setSingleProduct = (id) => {};
 
