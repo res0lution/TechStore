9aab77ef12c8c5e2f8f5e587e7eea5b06e6256f3
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-16 07:46:21 +0300

    Created CartTotals component

--- src/components/CartPage/CartTotals.jsx
+++ src/components/CartPage/CartTotals.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import { ProductConsumer } from "../../context/context";
 
 export const CartTotals = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {(value) => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value;
+
+            return (
+              <div className="col text-title text-center my-4">
+                <button
+                  className="btn btn-outline-danger text-capitalize mb-4"
+                  onClick={clearCart}
+                >
+                  Clear cart
+                </button>
+
+                <h3>Subtotal: ${cartSubTotal}</h3>
+                <h3>Tax: ${cartTax}</h3>
+                <h3>Total: ${cartTotal}</h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
+    </div>
+  );
+};
